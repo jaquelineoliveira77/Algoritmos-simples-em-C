5a21f27346c7a1fbf3a42ee24c0e326a2773c404
Jaqueline Oliveira <92550943+jaquelineoliveira77@users.noreply.github.com>
2022-08-22 23:01:14 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Função que imprimi uma mensagem de boas vindas
+void boasvindas(void);
+
+//função que imprimi a idade do usuario
+void imprimiridade (int i);
+
+
 int main()
 {
-    int x;
-    printf("Digite um numero inteiro qualquer\n");
-    scanf("%d",&x);
-    if (x%2==0)
-    {
-        printf("O numero %d e par\n",x);
-    }
-    else
-    {
-        printf("O numero %d e impar\n",x);
-    }
-
+    int idade;
+    boasvindas();
+    printf("Querido usuario, por favor digite a sua idade\n");
+    scanf("%d", &idade);
+    imprimiridade(idade);
     return 0;
+}
+
+void boasvindas(void)
+{
+    printf("Bem vindo ao meu programa\n");
+
+}
+
+
+void imprimiridade (int i)
+{
+    printf("Voce tem %d anos\n", i);
 }
